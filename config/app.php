f81f4ee1d7a479e2b382112283c0af1fd63d52ca
danitrimujianto <danitri33@gmail.com>
2019-01-02 00:46:42 +0700

    updet excel

--- config/app.php
+++ config/app.php
@@ -161,7 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-
+        Maatwebsite\Excel\ExcelServiceProvider::class,
         /*
          * Package Service Providers...
          */
@@ -226,6 +226,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'HelpMe' => App\Core\Libraries\FunctionGlobals::class,
         'HelpLocal' => App\Core\Libraries\FunctionLocals::class,
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
 
     ],
 
