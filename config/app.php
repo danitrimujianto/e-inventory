477391a0255f3c6ce030a7b92105a700dee647b1
danitrimujianto <danitri33@gmail.com>
2019-01-02 00:49:33 +0700

    updet excel

--- config/app.php
+++ config/app.php
@@ -161,7 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Maatwebsite\Excel\ExcelServiceProvider::class,
+        Maatwebsite\Excel\Src\ExcelServiceProvider::class,
         /*
          * Package Service Providers...
          */
