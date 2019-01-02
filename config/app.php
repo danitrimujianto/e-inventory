3a725530ac2dee0667398c544b4fd6d8dec1a385
danitrimujianto <danitri33@gmail.com>
2019-01-02 17:03:57 +0700

    updet excel

--- config/app.php
+++ config/app.php
@@ -164,6 +164,7 @@ return [
         /*
          * Package Service Providers...
          */
+         Maatwebsite\Excel\ExcelServiceProvider::class,
 
         /*
          * Application Service Providers...
