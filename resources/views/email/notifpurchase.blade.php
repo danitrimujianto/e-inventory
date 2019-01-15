f9e013495b4c4a7d43c7d1d4fd8eff20e6540c3f
danitrimujianto <danitri33@gmail.com>
2019-01-15 09:31:28 +0700

    updet notif

--- resources/views/email/notifpurchase.blade.php
+++ resources/views/email/notifpurchase.blade.php
@@ -1,8 +1,27 @@
+@php $total = ''; @endphp
 @component('mail::message')
-# Introduction
+# Request New Tools
 
-The body of your message.
+We recieved request new tools from:
+Name                  : {{ $data->karyawan->name }}
+Project               : {{ optional($data->karyawan->project)->name }}
+Departemen            : {{ optional($data->karyawan->departemen)->name }}
+Position              : {{ optional($data->karyawan->position)->position }}
+Assignment Area       : {{ optional($data->karyawan->assignmentarea)->name }}
 
+Request details
+----------------------------------------------------------------------------
+| Item | Merk   |   Type    |     Quantity    |   Price     |   Subtotal   |
+@component('mail::table')
+@foreach($detail AS $det)
+----------------------------------------------------------------------------
+| {{ $det->item }} | {{ $det->merk }} | {{ $det->type }} | {{ $det->quantity }} | {{ $det->price }} |
+----------------------------------------------------------------------------
+@php $total = $total+$det->price; @endphp
+@endforeach
+@endcomponent
+|                                                 Total      |     {{ $$total }}     |
+----------------------------------------------------------------------------
 @component('mail::button', ['url' => ''])
 Button Text
 @endcomponent
--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}">
-            {{ $slot }}
+            <img src="{{ asset('/dist/img/inventory-icon-white.png') }}" style=" width: 80px; height: 80px;"> {{ $slot }}
         </a>
     </td>
 </tr>
