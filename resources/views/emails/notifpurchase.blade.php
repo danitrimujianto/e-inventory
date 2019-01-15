c1ed5cc5d7f3fc0220f372e6e3e3f43b56b590a7
danitrimujianto <danitri33@gmail.com>
2019-01-15 10:00:30 +0700

    updet notif

--- resources/views/emails/notifpurchase.blade.php
+++ resources/views/emails/notifpurchase.blade.php
@@ -11,17 +11,31 @@ Assignment Area       : {{ optional($data->karyawan->assignmentarea)->name }}
 
 Request details
 
-@component('mail::table')
-| Item       | Type         | Merk  | Quantity  | Price  | Subtotal |
-@foreach($detail AS $det)
-| -----------|:------------:|:-----:|:---------:|:------:|:--------:|
-| {{ $det->item }} | {{ ($det->type ?? '') }} | {{ $det->merk }} | {{ $det->quantity }} | {{ HelpMe::cost2($det->price) }} |
-| -----------|:------------:|:-----:|:---------:|:------:|:--------:|
-@php $total = $total+$det->price; @endphp
-@endforeach
-|                                 Total      |     {{ HelpMe::cost2($total) }}     |
-| -----------|:------------:|:-----:|:---------:|:------:|:--------:|
-@endcomponent
+<table>
+  <tr>
+    <th>Item</th>
+    <th>Type</th>
+    <th>Merk</th>
+    <th>Quantity</th>
+    <th>Price</th>
+    <th>Subtotal</th>
+  </tr>
+  @foreach($detail AS $det)
+  <tr>
+    <td>{{ $det->item }}</td>
+    <td>{{ ($det->type ?? '') }}</td>
+    <td>{{ $det->merk }}</td>
+    <td>{{ $det->quantity }}</td>
+    <td>{{ HelpMe::cost2($det->price) }}</td>
+    <td>{{ HelpMe::cost2($det->total) }}</td>
+  </tr>
+  @php $total = $total+$det->price; @endphp
+  @endforeach
+  <tr>
+    <td colspan="5" align="right">Total:</td>
+    <td align="right">{{ HelpMe::cost2($total) }}</td>
+  </tr>
+</table>
 
 Please reponse this request.
 
