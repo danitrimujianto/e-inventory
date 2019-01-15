0d88ac7c8bf3cc56f9019f5c09e78c279c4ce9af
danitrimujianto <danitri33@gmail.com>
2019-01-15 10:46:11 +0700

    updet notif

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,7 +3,7 @@
       <table width="100%">
         <tr>
           <td width="50%" align="right"><img src="{{ asset('/dist/img/inventory-icon.png') }}" style=" width: 80px; height: 80px;"></td>
-          <td width="50%"><a href="{{ $url }}" style="font-size: 25px; color: #c51ae2;">
+          <td width="50%" align="left"><a href="{{ $url }}" style="font-size: 25px; color: #c51ae2;">
                {{ $slot }}
           </a></td>
         </tr>
