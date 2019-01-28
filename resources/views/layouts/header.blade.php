c2ae1ee49b62d3e5f24ea5f9cb3acb987797cfa9
danitrimujianto <danitri33@gmail.com>
2019-01-28 08:39:46 +0700

    updet employee

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -22,7 +22,7 @@
         <!-- User Account: style can be found in dropdown.less -->
         <li class="dropdown user user-menu">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-            <span class="hidden-xs">{{ ucwords(Auth::user()->name) }}</span>
+            <span class="">{{ ucwords(Auth::user()->name) }}</span>
           </a>
           <ul class="dropdown-menu">
             <!-- User image -->
