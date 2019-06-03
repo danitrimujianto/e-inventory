079d8d0388d6d5e0ca2f59df259585a1d529d7f3
danitrimujianto <danitri33@gmail.com>
2019-06-03 11:23:38 +0700

    Revisi mei

--- app/Core/Handlers/AcceptAllhoActivitiesHandler.php
+++ app/Core/Handlers/AcceptAllhoActivitiesHandler.php
@@ -97,7 +97,7 @@ class AcceptAllhoActivitiesHandler implements Handler
 
       $returnData['data'] = $data;
       $returnData['detail'] = $detailData;
-      $user = User::where('usertype_id', $target)->get();
+      $user = User::where('karyawan_id', $target)->get();
 
       $emails = array();
       foreach($user AS $val){
